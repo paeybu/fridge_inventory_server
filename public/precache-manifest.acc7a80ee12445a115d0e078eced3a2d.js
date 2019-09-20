1166b6e2204c3dcec8bad28bbb61734bb145bb9e
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2019-09-20 18:38:02 +0700

    add frontend

---
+++ public/precache-manifest.acc7a80ee12445a115d0e078eced3a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd29d34c19828b83ac4880b1fe1bb3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d743a35ac486c35c9903",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "50578b7fde4faa9eb705",
+    "url": "/static/css/main.f385225d.chunk.css"
+  },
+  {
+    "revision": "d743a35ac486c35c9903",
+    "url": "/static/js/2.4b1ad0c2.chunk.js"
+  },
+  {
+    "revision": "50578b7fde4faa9eb705",
+    "url": "/static/js/main.9931fb23.chunk.js"
+  },
+  {
+    "revision": "d706e5f346c3094ddd08",
+    "url": "/static/js/runtime~main.d82086a7.js"
+  }
+]);
